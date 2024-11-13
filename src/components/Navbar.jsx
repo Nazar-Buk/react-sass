5d2c0bd6f984ea712e44c96cea9c9540c7477a0d
Nazar Buk <nazarbuk27@gmail.com>
2024-11-13 22:07:38 +0200

    Add fonts
    ✏️ Add fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      Navbar
+      {/* //div.wrap-items>div.item${Item$}*4 */}
+      <div className="wrap-items">
+        <div className="item1">Item1</div>
+        <div className="item2">Item2</div>
+        <div className="item3">Item3</div>
+        <div className="item4">Item4</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
